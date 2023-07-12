bef895ec7fe02aef48597357936cdc88f9f3b102
SRINIVASAN S <101353273+srinivasansambath@users.noreply.github.com>
2023-07-12 22:14:21 +0530

    Various types of declaration of structures

---
+++ Anonymous structures.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    struct emp
+    {
+        int eid;
+        char ename[20];
+        float esal;
+    };
+    struct emp e1;
+}
+
+/*int main()
+{
+    struct emp
+    {
+        int eid;
+        char ename[20];
+        float esal;
+    }e1;
+}
+
+int main()
+{
+    struct
+    {
+        int eid;
+        char ename[20];
+        float esal;
+    }e1;
+
+}*/
